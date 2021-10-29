8c91856d85dcf1eb5bbe87ebd10c725be8e6ea99
jmolasky <jeromemolasky@gmail.com>
2021-10-28 23:54:06 -0500

    wip: improved functionality and added basic styles

--- script.js
+++ script.js
@@ -4,11 +4,11 @@ $(function(){
 
     // variables
     let cloudData;
-    let text;
+    let inputText;
     let fontFamily;
     let padding;
     let minWordLength;
-    let removeStopWords = false;
+    let removeStopwords = true;
     
     // cached element references
     const $form = $('form');
@@ -19,16 +19,29 @@ $(function(){
     $form.on('submit', handleSubmit);
 
     // functions
-
     function handleSubmit(evt) {
         evt.preventDefault();
-        text = $input.val();
+        //text = $input.val();
+        inputText = $input.val();
         $.ajax({
-            url: `${BASE_URL}?text=${text}&fontFamily=helvetica&padding=3&minWordLength=2&removeStopwords=true`,
+            url: BASE_URL,
+            data: {
+                "text": inputText,
+                "fontFamily": "helvetica",
+                "fontScale": "55",
+                "backgroundColor": "#535388",
+                "padding": "3",
+                "minWordLength": "2",
+                "rotation": "45",
+                "removeStopwords": removeStopwords
+            },
             dataType: 'html',
             success: function(data) {  
                 cloudData = data;       
                 render();
+            },
+            error: function(error) {
+                console.log("bad request: ", error);
             }
         });
         $input.val('');
@@ -36,9 +49,11 @@ $(function(){
 
     function render() {
         console.log(cloudData);
-        $main.append(cloudData);
+        $main.html(cloudData);
+        //$main.append(cloudData);
     }
 });
 
 // compare word clouds with each other?
 // upload files to be analyzed instead of inputting text by typing?
+// show or hide the form by clicking?
