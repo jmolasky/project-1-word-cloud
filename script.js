dc8e485ea043f339689ed34fdddf0998481c2d44
jmolasky <jeromemolasky@gmail.com>
2021-10-29 12:11:14 -0500

    wip: added .txt file upload functionality

--- script.js
+++ script.js
@@ -12,17 +12,21 @@ $(function(){
     
     // cached element references
     const $form = $('form');
-    const $input = $('input[type="text"]');
+    const $txtInput = $('input[type="text"]');
+    const $fileInput = $('input[type="file"]');
     const $main = $('main');
 
     // event listeners
     $form.on('submit', handleSubmit);
+    $fileInput.on('change', handleChange);
 
     // functions
     function handleSubmit(evt) {
         evt.preventDefault();
         //text = $input.val();
-        inputText = $input.val();
+        if($txtInput.val()) {
+            inputText = $txtInput.val();
+        }
         $.ajax({
             url: BASE_URL,
             data: {
@@ -44,7 +48,22 @@ $(function(){
                 console.log("bad request: ", error);
             }
         });
-        $input.val('');
+        $txtInput.val('');
+    }
+
+    function handleChange(evt) {
+        //evt.preventDefault();
+        const file = evt.target.files[0];
+        if(file) processFile(file);
+
+    }
+
+    function processFile(file) {
+        const fr = new FileReader();
+        fr.onload = function() {
+            inputText = fr.result;
+        }
+        fr.readAsText(file);
     }
 
     function render() {
@@ -57,3 +76,4 @@ $(function(){
 // compare word clouds with each other?
 // upload files to be analyzed instead of inputting text by typing?
 // show or hide the form by clicking?
+// have size of word cloud (width and height) adjust according to the size of the screen?
